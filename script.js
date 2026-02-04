4075b205c4b51e227d59bebf113f001a5f2b9a7f
DISCOUNTS-FROM-AL-LEXX <dyntchev@gmail.com>
2026-02-05 00:12:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -110,6 +110,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    setTimeout(async () => {
+        await sendForm();
+    }, 800); // 0.8 секунди
     // ─── Функція відправки форми (використовується і з кнопки, і з Enter) ──
     const sendForm = async () => {
         let link = field4.value.trim();
